5098700a4f9938d6395ab879c0676043f7eee587
cq01 <chq_github@outlook.com>
2019-10-24 17:56:58 +0800

    Homework 1791 - 2019级C++第一章探索性实验题(郑运平) 2527 Problem  A: 判断坐标区域

--- Contest.cpp
+++ Contest.cpp
@@ -1,8 +1,20 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
 #include <iostream>
-
+#include<cmath>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	int a, b;
+	while (cin >> a >> b)
+	{
+		if (fabs(a) <= 2 && fabs(b) <= 2)
+		{
+			cout << "true" << endl;
+		}
+		else
+		{
+			cout << "false" << endl;
+		}
+	}
 }
 
