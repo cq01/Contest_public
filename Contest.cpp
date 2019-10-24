7a947dec36b09c6d1f61abeb1045b6c3fa75230b
cq01 <chq_github@outlook.com>
2019-10-24 20:49:24 +0800

    Homework 1792 - 2019级C++第二章探索性实验题(郑运平) 2606 Problem  A: 菲波那契数列

--- Contest.cpp
+++ Contest.cpp
@@ -2,87 +2,24 @@
 #include <iostream>
 #include<cmath>
 using namespace std;
-bool able(const double& a, const double& b, const double& c, const double& d);
-bool sort(const double& a, const double& b, const double& c, const double& d);
-inline bool is24(const double& s);
-double op(int& mode, const double& a, const double& b);
+int fb(const int& n);
 int main()
 {
-	double a, b, c, d;
-	while (cin >> a >> b >> c >> d)
+	int n;
+	while (cin>>n)
 	{
-		if (sort(a, b, c, d))
-		{
-			cout << "YES" << endl;
-		}
-		else
-		{
-			cout << "NOT" << endl;
-		}
+		cout << fb(n) << endl;
 	}
+}
 
-}
-bool is24(const double& s)
+int fb(const int& n)
 {
-	return (fabs(s - 24) < 1e-6);
-}
-double op(int& mode, const double& a, const double& b)
-{
-	if (mode == 3 || b == 0)
+	int a = 1, b = 1,t ;
+	for (int i = 0; i < n; ++i)
 	{
-		return a;
+		t = a;
+		a = b;
+		b += t;
 	}
-	switch (mode)
-	{
-	case 0:return a + b; break;
-	case 1:return a - b; break;
-	case 2:return a * b; break;
-	case 3:return a / b; break;
-	}
-}
-bool able(const double& a, const double& b, const double& c, const double& d)
-{
-	for (int i = 0; i < 4; ++i)
-	{
-		for (int j = 0; j < 4; ++j)
-		{
-			for (int k = 0; k < 4; ++k)
-			{
-				//((a#b)#c)#d
-				if (is24(op(i, op(j, op(k, a, b), c), d)))
-				{
-					return true;
-				}
-			}
-		}
-	}
-	return false;
-}
-bool sort(const double& a, const double& b, const double& c, const double& d)
-{
-	double set[4] = { a,b,c,d };
-	for (int i = 0; i < 4; ++i)
-	{
-		for (int j = 0; j < 4; ++j)
-		{
-			if (j == i)
-			{
-				continue;
-			}
-			for (int k = 0; k < 4; ++k)
-			{
-				if (k == j || k == i)
-				{
-					continue;
-				}
-				int l = 6 - i - j - k;
-				if (able(set[i], set[j], set[k], set[l]))
-				{
-					return true;
-				}
-
-			}
-		}
-	}
-	return false;
+	return a;
 }
