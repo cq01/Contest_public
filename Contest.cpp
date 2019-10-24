cf87a99bd94402bb86e39e4ddb49c559d08e5f9d
cq01 <chq_github@outlook.com>
2019-10-24 21:30:58 +0800

    Homework 1792 - 2019级C++第二章探索性实验题(郑运平) 2611 Problem  C: 进制转换

--- Contest.cpp
+++ Contest.cpp
@@ -1,47 +1,13 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
 #include <iostream>
-#include<cmath>
+#include<iomanip>
+#include <bitset>
 using namespace std;
-void pr(const int& n);
 int main()
 {
 	int n;
-	while (cin>>n)
+	while (cin >> n)
 	{
-		pr(n);
+		cout <<' '<< bitset<8>(n) << ' ' << hex << setw(2) << setfill('0') << n <<endl;
 	}
-}
-void pr(const int& n)
-{
-	if (n == 1)
-	{
-		cout << "*\n";
-	}
-	else if (n == 2)
-	{
-		cout << "**\n**\n";
-	}
-	else
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			cout << '*';
-		}
-		cout << '\n';
-		for (int i = 0; i < n - 2; ++i)
-		{
-			cout << '*';
-			for (int j = 0; j < n - 2; ++j)
-			{
-				cout << ' ';
-			}
-			cout << "*\n";
-		}
-		for (int i = 0; i < n; ++i)
-		{
-			cout << '*';
-		}
-		cout << '\n';
-	}
-	cout<<'\n';
 }
