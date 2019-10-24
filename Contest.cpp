d62900c23e68f3638bdde7eb5b7f0384fa42a42d
cq01 <chq_github@outlook.com>
2019-10-24 21:41:11 +0800

    Homework 1792 - 2019级C++第二章探索性实验题(郑运平) 2605 Problem  D: 因数分解

--- Contest.cpp
+++ Contest.cpp
@@ -3,11 +3,30 @@
 #include<iomanip>
 #include <bitset>
 using namespace std;
+void apart(int& n);
 int main()
 {
 	int n;
 	while (cin >> n)
 	{
-		cout <<' '<< bitset<8>(n) << ' ' << hex << setw(2) << setfill('0') << n <<endl;
+		apart(n);
 	}
+}
+
+void apart(int& n)
+{
+	while (n>1)
+	{
+		for (int i = 2; i < 136; ++i)
+		{
+			if (n%i)
+			{
+				continue;
+			}
+			cout << ' ' << i;
+			n /= i;
+			break;
+		}
+	}
+	cout << endl;
 }
