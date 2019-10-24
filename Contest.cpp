24c0d67e485df33b903ab8da0c4205555d1cfd44
cq01 <chq_github@outlook.com>
2019-10-24 18:02:07 +0800

    Homework 1791 - 2019级C++第一章探索性实验题(郑运平) 2528 Problem  B: 条件表达式求最小值

--- Contest.cpp
+++ Contest.cpp
@@ -1,20 +1,13 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
 #include <iostream>
-#include<cmath>
 using namespace std;
 int main()
 {
-	int a, b;
-	while (cin >> a >> b)
+	int a, b, c, s;
+	while (cin>>a>>b>>c)
 	{
-		if (fabs(a) <= 2 && fabs(b) <= 2)
-		{
-			cout << "true" << endl;
-		}
-		else
-		{
-			cout << "false" << endl;
-		}
+		s = a > b ? (b > c ? c : b) : (a > c ? c : a);
+		cout << s << endl;
 	}
 }
 
