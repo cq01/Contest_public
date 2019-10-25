4ed71ed379f53b59a63c75ddd9b9523d5a22e0d1
cq01 <chq_github@outlook.com>
2019-10-25 08:02:58 +0800

    Homework 1791 - 2019级C++第三章探索性实验题(郑运平) 2632 Problem  A: 勒让德多项式

--- Contest.cpp
+++ Contest.cpp
@@ -1,32 +1,32 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
 #include <iostream>
 #include<iomanip>
-#include <bitset>
 using namespace std;
-void apart(int& n);
+//如果写double Pfunc(double x, int n); OJ会报编译错误
+double Pfunc(double x, int n)
+{
+	if (!n)//n==0
+	{
+		return 1;
+	}
+	if (n == 1)
+	{
+		return x;
+	}
+	return ((2l * n - 1) * Pfunc(x, n - 1) - (n - 1l) * Pfunc(x, n - 2)) / n;
+}
 int main()
 {
 	int n;
-	while (cin >> n)
+	double x;
+	float xf;
+	while (cin>>xf>>n)
 	{
-		apart(n);
+		x = double(xf);
+		cout << fixed << setprecision(2) << Pfunc(x, n) << endl;
 	}
 }
-
-void apart(int& n)
-{
-	while (n>1)
-	{
-		for (int i = 2; i < 136; ++i)
-		{
-			if (n%i)
-			{
-				continue;
-			}
-			cout << ' ' << i;
-			n /= i;
-			break;
-		}
-	}
-	cout << endl;
-}
+//P[n](x)
+//{1, n = 0；
+//{ x ,n = 1；
+//{((2n - 1)P[n - 1](x)) - (n - 1)P[n - 2](x)) / n ,n > 1；
