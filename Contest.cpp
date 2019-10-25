b9b43b1bf60f0aad41a1295070abf0a9fe8de676
cq01 <chq_github@outlook.com>
2019-10-25 16:30:35 +0800

    Homework 1793 - 2019级C++第三章探索性实验题(郑运平) 2639 Problem  C: 蛇形矩阵

--- Contest.cpp
+++ Contest.cpp
@@ -1,31 +1,58 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
 #include <iostream>
-#include<cmath>
-#include<iomanip>
 using namespace std;
-
-
-double sum(const int& n, const int& q);
+void pr(const int& n);
 int main()
 {
-	int m, n, p;
-	double s = 0;
-	while (cin >> m >> n >> p)
+	int n;
+	while (cin >> n)
 	{
-		s += sum(m, 1);
-		s += sum(n, 3);
-		s /= sum(p, 5);
-		cout << fixed << setprecision(4) << s << endl;
-		s = 0;
+		pr(n);
 	}
 }
-
-double sum(const int& n, const int& q)
+void pr(const int& n)
 {
-	double result = 0;
+	int res[50][50] = { 0 };
+	int j = 0, k = 0;
 	for (int i = 1; i <= n; ++i)
 	{
-		result += pow(i, q);
+		res[j][k] = i;
+		if (j == 0)
+		{
+			j = k + 1;
+			k = 0;
+		}
+		else
+		{
+			--j;
+			++k;
+		}
 	}
-	return result;
+	for (int j = 0; j < 50; ++j)
+	{
+		if (res[j][0])
+		{
+			for (int k = 0; k < 50; ++k)
+			{
+
+				if (res[j][k])
+				{
+					if (k)
+					{
+						cout << ' ';
+					}
+					cout << res[j][k];
+				}
+				else
+				{
+					cout << endl;
+					break;
+				}
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
 }
