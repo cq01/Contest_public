c5ea97be9cdd0637505bf667eda305abc5608326
cq01 <chq_github@outlook.com>
2019-10-25 15:21:17 +0800

    Homework 1793 - 2019级C++第三章探索性实验题(郑运平) 2638 Problem  B: 计算数值

--- Contest.cpp
+++ Contest.cpp
@@ -1,93 +1,31 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
 #include <iostream>
 #include<cmath>
+#include<iomanip>
 using namespace std;
-bool able(const double& a, const double& b, const double& c, const double& d);
-bool sort(const double& a, const double& b, const double& c, const double& d);
-inline bool is24(const double& s);
-double op(int& mode, const double& a, const double& b);
+
+
+double sum(const int& n, const int& q);
 int main()
 {
-	double a, b, c, d;
-	while (cin >> a >> b >> c >> d)
+	int m, n, p;
+	double s = 0;
+	while (cin >> m >> n >> p)
 	{
-		if (sort(a, b, c, d))
-		{
-			cout << "YES" << endl;
-		}
-		else
-		{
-			cout << "NOT" << endl;
-		}
+		s += sum(m, 1);
+		s += sum(n, 3);
+		s /= sum(p, 5);
+		cout << fixed << setprecision(4) << s << endl;
+		s = 0;
 	}
+}
 
-}
-bool is24(const double& s)
+double sum(const int& n, const int& q)
 {
-	return (fabs(s - 24) < 1e-6);
-}
-double op(int& mode, const double& a, const double& b)
-{
-	if (mode == 3 || b == 0)
+	double result = 0;
+	for (int i = 1; i <= n; ++i)
 	{
-		return a;
+		result += pow(i, q);
 	}
-	switch (mode)
-	{
-	case 0:return a + b; break;
-	case 1:return a - b; break;
-	case 2:return a * b; break;
-	case 3:return a / b; break;
-	}
-}
-bool able(const double& a, const double& b, const double& c, const double& d)
-{
-	for (int i = 0; i < 4; ++i)
-	{
-		for (int j = 0; j < 4; ++j)
-		{
-			for (int k = 0; k < 4; ++k)
-			{
-				//((a#b)#c)#d
-				if (is24(op(i, op(j, op(k, a, b), c), d)))
-				{
-					return true;
-				}
-				//(a#b)#(c#d)
-				if (is24(op(i, op(j,a,b),op(k,c,d))))
-				{
-					return true;
-				}
-			}
-		}
-	}
-	return false;
-}
-bool sort(const double& a, const double& b, const double& c, const double& d)
-{
-	double set[4] = { a,b,c,d };
-	for (int i = 0; i < 4; ++i)
-	{
-		for (int j = 0; j < 4; ++j)
-		{
-			if (j == i)
-			{
-				continue;
-			}
-			for (int k = 0; k < 4; ++k)
-			{
-				if (k == j || k == i)
-				{
-					continue;
-				}
-				int l = 6 - i - j - k;
-				if (able(set[i], set[j], set[k], set[l]))
-				{
-					return true;
-				}
-
-			}
-		}
-	}
-	return false;
+	return result;
 }
