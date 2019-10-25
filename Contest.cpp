d03676a018317c7ae171bc37a2c1bce95c7962e9
cq01 <chq_github@outlook.com>
2019-10-25 23:38:57 +0800

    Homework 1795 - 2019级C++第五章探索性实验题(郑运平) 2680 Problem  A: 找最高分同学

--- Contest.cpp
+++ Contest.cpp
@@ -1,40 +1,27 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
 #include<iostream>
-#include<string>
-#include<cstring>
 using namespace std;
+
+struct student
+{
+	int record;
+	char name[128];
+};
 int main()
 {
-
-	char s[128];
-	char op[4];
-	int x, n;
-	cin >> s;
-	string str(s);
-	while (cin >> op)
+	student stu[10];
+	for (int i = 0; i < 10; ++i)
 	{
-		if (!strcmp(op, "ADD"))
+		cin >> stu[i].name >> stu[i].record;
+	}
+	int a = 0, max = 0;
+	for (int j = 0; j < 10; ++j)
+	{
+		if (stu[j].record > max)
 		{
-			cin >> s >> x;
-			str.insert(++x, s);
-			cout << str << endl;
-		}
-		else if (!strcmp(op, "DEL"))
-		{
-			cin >> x >> n;
-			str.erase(x, n);
-			cout << str << endl;
-		}
-		else if (!strcmp(op, "SER"))
-		{
-			cin >> s;
-			cout << str.find(s) << endl;
-		}
-		else
-		{
-			cin >> s >> x;
-			str.replace(x, strlen(s), s);
-			cout << str << endl;
+			max = stu[j].record;
+			a = j;
 		}
 	}
+	cout << stu[a].name << endl;
 }
