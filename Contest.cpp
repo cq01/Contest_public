687d81f80fb7504b930e6ccc15131ee07b626eb2
cq01 <chq_github@outlook.com>
2019-10-25 22:08:39 +0800

    Homework 1794 - 2019级C++第四章探索性实验题(郑运平) 2666 Problem  A: 矩阵求和

--- Contest.cpp
+++ Contest.cpp
@@ -1,31 +1,68 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
 #include <iostream>
-#include<cstring>
 using namespace std;
-int test(char str[]);
+void fillmartix(const int& n, int p[]);
+void printmartix(const int& n, int p[]);
+int atr(const int& n);
+inline int op(const int& a, const int& b, const int& n);
+inline int value(const int& n);
+inline int sum2(const int& n);
 int main()
 {
-	char str[1024];
-	while (cin >> str)
+	int n;
+	while (cin >> n)
 	{
-		if (test(str))
+		int* p = new int[n * n]();
+		fillmartix(n, p);
+		printmartix(n, p);
+		cout << atr(n) << endl;
+	}
+}
+inline int value(const int& n)
+{
+	return n > 1 ? n : 1;
+}
+int op(const int& a, const int& b, const int& n)
+{
+	return a * n + b;
+}
+
+void fillmartix(const int& n, int p[])
+{
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
 		{
-			cout << "NO" << endl;
-		}
-		else
-		{
-			cout << "YES" << endl;
+			p[op(i, j, n)] = value(i + 1 - j);
 		}
 	}
 }
 
-int test(char str[])
+void printmartix(const int& n, int p[])
 {
-	int n = strlen(str);
-	int s = 0;
-	for (int i = 0; i < n / 2; ++i)
+	for (int i = 0; i < n; ++i)
 	{
-		s += (str[i] != str[n - i - 1]);
+		for (int j = 0; j < n; ++j)
+		{
+			if (j)
+			{
+				cout << ' ';
+			}
+			cout << p[op(i, j, n)];
+		}
+		cout << endl;
+	}
+}
+int atr(const int& n)
+{
+	int s = 0;
+	for (int i = 2; i <= n; ++i)
+	{
+		s += sum2(i);
 	}
 	return s;
+}
+int sum2(const int& n)
+{
+	return (n - 1) * (n + 2) / 2;
 }
