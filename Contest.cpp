45108c765cf39d057f1e9859a4d60b018eb849e1
cq01 <chq_github@outlook.com>
2019-10-25 22:15:43 +0800

    Homework 1794 - 2019级C++第四章探索性实验题(郑运平) 2672 Problem  B: 数组输出

--- Contest.cpp
+++ Contest.cpp
@@ -1,68 +1,17 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
 #include <iostream>
 using namespace std;
-void fillmartix(const int& n, int p[]);
-void printmartix(const int& n, int p[]);
-int atr(const int& n);
-inline int op(const int& a, const int& b, const int& n);
-inline int value(const int& n);
-inline int sum2(const int& n);
 int main()
 {
-	int n;
-	while (cin >> n)
+	int s[10] = { 0 };
+	int a;
+	for (int i = 0; i < 10; ++i)
 	{
-		int* p = new int[n * n]();
-		fillmartix(n, p);
-		printmartix(n, p);
-		cout << atr(n) << endl;
+		cin >> a;
+		s[a] = i;
 	}
-}
-inline int value(const int& n)
-{
-	return n > 1 ? n : 1;
-}
-int op(const int& a, const int& b, const int& n)
-{
-	return a * n + b;
-}
-
-void fillmartix(const int& n, int p[])
-{
-	for (int i = 0; i < n; ++i)
+	for (int i = 9; i >=0; --i)
 	{
-		for (int j = 0; j < n; ++j)
-		{
-			p[op(i, j, n)] = value(i + 1 - j);
-		}
+		cout << i << ' ' << s[i] << endl;
 	}
-}
-
-void printmartix(const int& n, int p[])
-{
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-			if (j)
-			{
-				cout << ' ';
-			}
-			cout << p[op(i, j, n)];
-		}
-		cout << endl;
-	}
-}
-int atr(const int& n)
-{
-	int s = 0;
-	for (int i = 2; i <= n; ++i)
-	{
-		s += sum2(i);
-	}
-	return s;
-}
-int sum2(const int& n)
-{
-	return (n - 1) * (n + 2) / 2;
 }
