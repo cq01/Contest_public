bfaac9dc9961592ef3d442f3b86342a6c80afe20
cq01 <chq_github@outlook.com>
2019-10-25 08:13:45 +0800

    Homework 1791 - 2019级C++第一章探索性实验题(郑运平) 2534 Problem  D: 24点 bugfix1

--- Contest.cpp
+++ Contest.cpp
@@ -1,32 +1,93 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
 #include <iostream>
-#include<iomanip>
+#include<cmath>
 using namespace std;
-//如果写double Pfunc(double x, int n); OJ会报编译错误
-double Pfunc(double x, int n)
-{
-	if (!n)//n==0
-	{
-		return 1;
-	}
-	if (n == 1)
-	{
-		return x;
-	}
-	return ((2l * n - 1) * Pfunc(x, n - 1) - (n - 1l) * Pfunc(x, n - 2)) / n;
-}
+bool able(const double& a, const double& b, const double& c, const double& d);
+bool sort(const double& a, const double& b, const double& c, const double& d);
+inline bool is24(const double& s);
+double op(int& mode, const double& a, const double& b);
 int main()
 {
-	int n;
-	double x;
-	float xf;
-	while (cin>>xf>>n)
+	double a, b, c, d;
+	while (cin >> a >> b >> c >> d)
 	{
-		x = double(xf);
-		cout << fixed << setprecision(2) << Pfunc(x, n) << endl;
+		if (sort(a, b, c, d))
+		{
+			cout << "YES" << endl;
+		}
+		else
+		{
+			cout << "NOT" << endl;
+		}
+	}
+
+}
+bool is24(const double& s)
+{
+	return (fabs(s - 24) < 1e-6);
+}
+double op(int& mode, const double& a, const double& b)
+{
+	if (mode == 3 || b == 0)
+	{
+		return a;
+	}
+	switch (mode)
+	{
+	case 0:return a + b; break;
+	case 1:return a - b; break;
+	case 2:return a * b; break;
+	case 3:return a / b; break;
 	}
 }
-//P[n](x)
-//{1, n = 0；
-//{ x ,n = 1；
-//{((2n - 1)P[n - 1](x)) - (n - 1)P[n - 2](x)) / n ,n > 1；
+bool able(const double& a, const double& b, const double& c, const double& d)
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			for (int k = 0; k < 4; ++k)
+			{
+				//((a#b)#c)#d
+				if (is24(op(i, op(j, op(k, a, b), c), d)))
+				{
+					return true;
+				}
+				//(a#b)#(c#d)
+				if (is24(op(i, op(j,a,b),op(k,c,d))))
+				{
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+bool sort(const double& a, const double& b, const double& c, const double& d)
+{
+	double set[4] = { a,b,c,d };
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			if (j == i)
+			{
+				continue;
+			}
+			for (int k = 0; k < 4; ++k)
+			{
+				if (k == j || k == i)
+				{
+					continue;
+				}
+				int l = 6 - i - j - k;
+				if (able(set[i], set[j], set[k], set[l]))
+				{
+					return true;
+				}
+
+			}
+		}
+	}
+	return false;
+}
