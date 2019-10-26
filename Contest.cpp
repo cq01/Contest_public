ca33fe4601b808d14f649d4195072dfeed6629ee
cq01 <chq_github@outlook.com>
2019-10-26 20:48:57 +0800

    Homework 1795 - 2019级C++第五章探索性实验题(郑运平) 2682 Problem  B: 输出链表字符

--- Contest.cpp
+++ Contest.cpp
@@ -1,27 +1,66 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
-#include<iostream>
+#include <iostream>
+#include <cstring>
 using namespace std;
-
-struct student
+struct number
 {
-	int record;
-	char name[128];
+	number* next;
+	char data;
 };
+void creat(number*& head, char* s);
+void readDelete(number* &head);
 int main()
 {
-	student stu[10];
-	for (int i = 0; i < 10; ++i)
+	number* head = nullptr;
+	char s[256] = { 0 };
+	while (cin >> s)
 	{
-		cin >> stu[i].name >> stu[i].record;
+		creat(head, s);
+		readDelete(head);
 	}
-	int a = 0, max = 0;
-	for (int j = 0; j < 10; ++j)
+
+}
+
+void creat(number*& head, char* s)
+{
+	number* p = nullptr, * last = nullptr;
+	int n = (int)strlen(s);
+	for (int i = n - 1; i >= 0; --i)
 	{
-		if (stu[j].record > max)
+		p = last;
+		last = new number;
+		last->data = *(s + i);
+		if (head == nullptr)
 		{
-			max = stu[j].record;
-			a = j;
+			head = last;
+			p = head;
+		}
+		else
+		{
+			if (p != nullptr)
+			{
+				p->next = last;
+			}
 		}
 	}
-	cout << stu[a].name << endl;
+	if (last != nullptr)
+	{
+		last->next = nullptr;
+	}
+	p = nullptr;
+	last = nullptr;
+}
+void readDelete(number* &head)
+{
+	number* pt = nullptr;
+	while (head != nullptr)
+	{
+		cout << head->data;
+		pt = head;
+		head = pt->next;
+		delete pt;
+	}
+	pt = nullptr;
+	head = nullptr;// head 不为nullptr 下次调用会有异常
+	cout << endl;
 }
