82584573c19440557d1acaab23743d85d42c586e
cq01 <chq_github@outlook.com>
2019-12-18 23:28:39 +0800

    3483 Problem  E 孪生数字

--- main.cpp
+++ main.cpp
@@ -1,31 +1,65 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
-#include <iostream>
-#include <cstring>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+
+#define clear_array(x) memset((x),0,sizeof(x))
+bool Test();
 int main()
 {
-	int n, T;
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	int T;
 	while (cin >> T)
 	{
 		for (int t = 0; t < T; ++t)
 		{
-			cin >> n;
-			for (int i = 0; i < n; ++i)
+			if (Test())
 			{
-				for (int j = 0; j < n; ++j)
-				{
-					if (i == j || i + j == n - 1)
-					{
-						cout << 'X';
-					}
-					else if ((i + j < n - 1) || (j < i))
-					{
-						cout << ' ';
-					}
-				}
-				cout << endl;
+				cout << "Yes" << endl;
+			}
+			else
+			{
+				cout << "No" << endl;
 			}
 		}
 	}
+}
+
+bool Test()
+{
+	char numstr[21] = { 0 };
+	uint8_t numarray[20] = { 0 };
+	clear_array(numstr);
+	clear_array(numarray);
+	cin >> numstr;
+	uint8_t num1count[10] = { 0 }, num2count[10] = { 0 };
+	clear_array(num1count);
+	clear_array(num2count);
+
+	for (int i = strlen(numstr) - 1, j = 0; i >= 0; --i)
+	{
+		numarray[j++] = uint8_t(numstr[i] - '0');
+	}
+	for (uint8_t& x : numarray)
+	{
+		++num1count[x];
+	}
+	uint8_t jin = 0;
+	for (uint8_t& x : numarray)
+	{
+		x = x * 2 + jin;
+		jin = x / 10;
+		x %= 10;
+	}
+	for (uint8_t& x : numarray)
+	{
+		++num2count[x];
+	}
+	bool isConsist = true;
+	for (int i = 0; i < 10; ++i)
+	{
+		isConsist = isConsist && (num1count[i] == num2count[i]);
+	}
+	return isConsist;
 }
