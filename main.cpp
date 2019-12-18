b1ff9a4f7b5b50a8c4feafa4f6b697e0734ecf0c
cq01 <chq_github@outlook.com>
2019-12-18 22:09:54 +0800

    3469 Problem  C 整数去重

--- main.cpp
+++ main.cpp
@@ -2,50 +2,34 @@
 #include <iostream>
 #include <cstring>
 #include <algorithm>
+#define clearA(a) memset((a),0,sizeof(a))
 using namespace std;
-const int tableSize = 25;
-const int primeTable[tableSize] = { 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97 };
-bool isPrime(int x)
-{
-	for (int i = 0; i < tableSize; ++i)
-	{
-		if (x == primeTable[i])
-		{
-			return true;
-		}
-	}
-	return false;
-}
 int main()
 {
-	char str[128];
-	while (cin >> str)
+	int n;
+	while (cin >> n)
 	{
-		int num = strlen(str);
-		int times[26] = { 0,0,0,0,0,0 };
-		memset(times, 0, sizeof(times));
-		for (int i = 0; i < num; ++i)
+		short Tlist[20000];
+		bool inL[5000];
+		clearA(inL);
+		clearA(Tlist);
+		int in;
+		int m = 0;
+		for (int i = 0; i < n; ++i)
 		{
-			++times[str[i] - 'a'];
-		}
-		int minN = 200, maxN = 0;
-		for (int i = 0; i < 26; ++i)
-		{
-			if (times[i])
-			{
-				minN = min(minN, times[i]);
-				maxN = max(maxN, times[i]);
-			}
 
+			cin >> in;
+			if (!(inL[in]))
+			{
+				inL[in] = true;
+				Tlist[m++] = in;
+			}
 		}
-		int LW = maxN - minN;
-		if (isPrime(LW))
+
+		for (int i = 0; i < m; ++i)
 		{
-			cout << "Lucky Word\n" << LW << endl;
+			cout << Tlist[i] << ' ';
 		}
-		else {
-			cout << "No Answer\n0" << endl;
-		}
+		cout << endl;
 	}
 }
-
