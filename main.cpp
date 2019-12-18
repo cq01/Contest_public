81db300d668ed340a77f5f3cfffda3de2265232c
cq01 <chq_github@outlook.com>
2019-12-18 23:44:49 +0800

    3484 Problem  F 句子逆序

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 #define clear_array(x) memset((x),0,sizeof(x))
-bool Test();
+
 int main()
 {
 	std::ios::sync_with_stdio(false);
@@ -12,54 +12,27 @@ int main()
 	int T;
 	while (cin >> T)
 	{
-		for (int t = 0; t < T; ++t)
+		for (int i = 0; i < T; ++i)
 		{
-			if (Test())
+			stack<string> str_stack;
+			string str;
+			char c;
+			do
 			{
-				cout << "Yes" << endl;
-			}
-			else
+				cin >> str;
+				str_stack.push(str);
+				c = cin.get();
+			} while (c != '\n');
+			do
 			{
-				cout << "No" << endl;
-			}
+				cout << str_stack.top();
+				str_stack.pop();
+				if (!(str_stack.empty()))
+				{
+					cout << ' ';
+				}
+			} while (!(str_stack.empty()));
+			cout << endl;
 		}
 	}
-}
-
-bool Test()
-{
-	char numstr[21] = { 0 };
-	uint8_t numarray[20] = { 0 };
-	clear_array(numstr);
-	clear_array(numarray);
-	cin >> numstr;
-	uint8_t num1count[10] = { 0 }, num2count[10] = { 0 };
-	clear_array(num1count);
-	clear_array(num2count);
-
-	for (int i = strlen(numstr) - 1, j = 0; i >= 0; --i)
-	{
-		numarray[j++] = uint8_t(numstr[i] - '0');
-	}
-	for (uint8_t& x : numarray)
-	{
-		++num1count[x];
-	}
-	uint8_t jin = 0;
-	for (uint8_t& x : numarray)
-	{
-		x = x * 2 + jin;
-		jin = x / 10;
-		x %= 10;
-	}
-	for (uint8_t& x : numarray)
-	{
-		++num2count[x];
-	}
-	bool isConsist = true;
-	for (int i = 0; i < 10; ++i)
-	{
-		isConsist = isConsist && (num1count[i] == num2count[i]);
-	}
-	return isConsist;
 }
