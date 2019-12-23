a2c11523323f01dfe1c57c251a5da071dcbbf8f5
cq01 <chq_github@outlook.com>
2019-12-23 15:53:53 +0800

    3519 Problem  F 怪兽攻击

--- main.cpp
+++ main.cpp
@@ -2,52 +2,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+#define clr(A) memset((A),0,sizeof(A))
+int search(int k);
 int main()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	std::cout.tie(nullptr);
-	int n, m;
-	while (cin >> n >> m)
+	int n;
+	while (cin >> n)
 	{
-		priority_queue<int, vector<int>, greater<int> > N, M;
-		int x;
-		//in
-		while (n--)
+		vector<int> T(n);
+		for (int i = 0; i < n; i++)
 		{
-			cin >> x;
-			N.push(x);
+			cin >> T[i];
 		}
-		while (m--)
+		int t;
+		cin >> t;
+		int sum = T[n - 1] + t;
+		bool* yun = new bool[sum + 1];
+		memset(yun, 0, (sum + 1) * sizeof(bool));
+		for (int& t0 : T)
 		{
-			cin >> x;
-			M.push(x);
-		}
-		//out
-		bool notfirst = false;
-		while (!(N.empty()||M.empty()))
-		{
-			if (N.top()==M.top())
+			for (int i = t0; i < t0 + t; i++)
 			{
-				if (notfirst)
-				{
-					cout << ' ';
-				}
-				notfirst = true;
-				cout << N.top();
-				N.pop();
-				M.pop();
-			}
-			else if (N.top()<M.top())
-			{
-				N.pop();
-			}
-			else if (N.top()>M.top())
-			{
-				M.pop();
+				yun[i] = true;
 			}
 		}
-		cout << endl;
+		int Tsum = 0;
+		for (int i = 0; i < sum + 1; i++)
+		{
+			Tsum += yun[i];
+		}
+		cout << Tsum << endl;
+		delete[] yun;
 	}
 }
+
