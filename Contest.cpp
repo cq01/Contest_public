f700e41d3ff6fd8b2bb3773eed37805a09f2643a
cq01 <chq_github@outlook.com>
2020-03-05 22:43:38 +0800

    Homework 1841 - 2019级C++第七章探索性实验题(郑运平) 2744 Problem  D 4. 句子逆序

--- Contest.cpp
+++ Contest.cpp
@@ -5,61 +5,7 @@ using ll = long long;
 #define clr_0(A) memset((A),0,sizeof(A))
 //#define DEBUG
 
-class Vector
-{
-private:
-	vector<ll> val;
-public:
-	void read(size_t num)
-	{
-		val.clear();
-		val.resize(num);
-		for (auto& v : val) cin >> v;
-	}
-	void print()
-	{
-		for (int i = 0; i < val.size(); ++i)
-		{
-			if (i)cout << ' ';
-			cout << val[i];
-		}
-		cout << endl;
-	}
-	friend Vector operator+(const Vector& a, const Vector& b);
-	friend Vector operator-(const Vector& a, const Vector& b);
-	friend ll operator*(const Vector& a, const Vector& b);
-};
-Vector operator+(const Vector& a, const Vector& b)
-{
-	Vector ret;
-	ret.val.clear();
-	ret.val.resize(a.val.size());
-	for (int i = 0; i < ret.val.size(); ++i)
-	{
-		ret.val[i] = a.val[i] + b.val[i];
-	}
-	return ret;
-}
-Vector operator-(const Vector& a, const Vector& b)
-{
-	Vector ret;
-	ret.val.clear();
-	ret.val.resize(a.val.size());
-	for (int i = 0; i < ret.val.size(); ++i)
-	{
-		ret.val[i] = a.val[i] - b.val[i];
-	}
-	return ret;
-}
-ll operator*(const Vector& a, const Vector& b)
-{
-	ll ret = 0;
-	for (int i = 0; i < a.val.size(); ++i)
-	{
-		ret += a.val[i] * b.val[i];
-	}
-	return ret;
-}
+
 int main()
 {
 #ifdef DEBUG
@@ -77,20 +23,17 @@ int main()
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	std::cout.tie(nullptr);
-	char op;
-	size_t num;
-	Vector a, b;
-	while (cin >> op >> num)
+	string str;
+	stack<string> s;
+	while (cin >> str)
 	{
-		a.read(num);
-		b.read(num);
-		switch (op)
-		{
-		case '+':(a + b).print(); break;
-		case '-':(a - b).print(); break;
-		case '*':cout << a * b << endl; break;
-		default:
-			break;
-		}
+		s.push(str);
 	}
+	while (!s.empty())
+	{
+		cout << s.top();
+		s.pop();
+		if (!s.empty())cout << ' ';
+	}
+	cout << endl;
 }
