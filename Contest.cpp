502062f4e025685fa8fb06d5d1ccb765a0461d84
cq01 <chq_github@outlook.com>
2020-03-05 23:17:49 +0800

    Homework 1842 - 2019级C++第八章探索性实验题(郑运平) 2770 Problem  A 1. 工资计算类

--- Contest.cpp
+++ Contest.cpp
@@ -5,7 +5,67 @@ using ll = long long;
 #define clr_0(A) memset((A),0,sizeof(A))
 //#define DEBUG
 
-
+class Employee
+{
+protected:
+	string name, number;
+	double basicSalary;
+public:
+	Employee() :basicSalary(2000) {}
+	void input()
+	{
+		cin >> number >> name;
+	}
+	double pay()
+	{
+		return basicSalary;
+	}
+	void print()
+	{
+		cout << name << ' ' << number << ' ' << pay() << endl;
+	}
+};
+class Salesman :protected Employee
+{
+protected:
+	double sales;
+	static double commrate;
+public:
+	Salesman() :sales() {}
+	void input()
+	{
+		Employee::input();
+		cin >> sales;
+	}
+	double pay()
+	{
+		return Employee::pay() + sales * commrate;
+	}
+	void print()
+	{
+		cout << name << ' ' << number << ' ' << pay() << endl;
+	}
+};
+double Salesman::commrate = 5e-3;
+class Salesmanager :protected Salesman
+{
+protected:
+	double jobSalary;
+public:
+	Salesmanager() :jobSalary(3000) {}
+	void input()
+	{
+		Salesman::input();
+	}
+	double pay()
+	{
+		return Salesman::pay() + jobSalary;
+	}
+	void print()
+	{
+		cout << name << ' ' << number << ' ' << pay() << endl;
+	}
+};
 int main()
 {
 #ifdef DEBUG
@@ -23,17 +83,13 @@ int main()
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	std::cout.tie(nullptr);
-	string str;
-	stack<string> s;
-	while (cin >> str)
-	{
-		s.push(str);
-	}
-	while (!s.empty())
-	{
-		cout << s.top();
-		s.pop();
-		if (!s.empty())cout << ' ';
-	}
-	cout << endl;
+	Employee a;
+	Salesman b;
+	Salesmanager c;
+	a.input();
+	b.input();
+	c.input();
+	a.print();
+	b.print();
+	c.print();
 }
