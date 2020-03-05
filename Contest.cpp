dbfa495f376ebf7045fca93d6b2bd811f8ca1f96
cq01 <chq_github@outlook.com>
2020-03-05 21:35:03 +0800

    Homework 1840 - 2019级C++第六章探索性实验题(郑运平) 2737 Problem  A 1. 图书类

--- Contest.cpp
+++ Contest.cpp
@@ -1,87 +1,29 @@
 ﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
 #include <bits/stdc++.h>
 using namespace std;
-#define DEBUG
-#define clr(x) memset((x),0,sizeof(x))
+using ll = long long;
+//#define DEBUG
 
-const int horizontal[8] = { 2,1,-1,-2,-2,-1,1,2 };
-const int vertical[8] = { -1,-2,-2,-1,1,2,2,1 };
-
-bool S[500][500] = {};
-unsigned int L[500][500] = {};
-
-struct Point
+class Book
 {
-	int x, y;
-	unsigned int step;
-	bool inS()
+public:
+	string bookname;
+	ll price, number;
+	Book() :price(), number() {}
+	ll display()
 	{
-		return S[x][y];
+		return price * number;
 	}
-	void setS()
+	ll borrow()
 	{
-		S[x][y] = true;
+		return --number;
 	}
-	void setL()
+	ll restore()
 	{
-		step = min(step, L[x][y]);
-		L[x][y] = step;
-	}
-	Point& equals(Point& current, int i)
-	{
-		x = current.x + horizontal[i];
-		y = current.y + vertical[i];
-		step = current.step + 1;
-		setL();
-		return *this;
-	}
-
-	Point(int x = 0, int y = 0, int step = 65535) :x(x), y(y), step(step)
-	{
-		setL();
-	}
-	Point(Point& current, int i)
-	{
-		this->equals(current, i);
-	}
-	Point& operator =(Point& current)
-	{
-		x = current.x;
-		y = current.y;
-		step = current.step;
-		return *this;
-	}
-	Point& operator =(const Point& current)
-	{
-		x = current.x;
-		y = current.y;
-		step = current.step;
-		return *this;
+		return ++number;
 	}
 };
-bool operator ==(Point A, Point B)
-{
-	return (A.x == B.x) && (A.y == B.y);
-}
-bool operator !=(Point A, Point B)
-{
-	return !(A == B);
-}
-bool operator < (Point A, Point B)
-{
-	return A.step < B.step;
-}
-bool operator > (Point A, Point B)
-{
-	return A.step > B.step;
-}
-void p(Point& current)
-{
-	cout << setw(4) << current.x << setw(4) << current.y << setw(4) << current.step << endl;
-}
-void span(int& m, int& n, int& a, int& b, int& c, int& d);
-void reduce(int& m, int& n, int& a, int& b, int& c, int& d);
-int search(int m, int n, int a, int b, int c, int d);
+
 int main()
 {
 #ifdef DEBUG
@@ -99,151 +41,25 @@ int main()
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	std::cout.tie(nullptr);
-	int k;
-	while (cin >> k)
+	Book aBook;
+	char op;
+	while (cin >> aBook.bookname >> aBook.price >> aBook.number)
 	{
-		while (k--)
+		cout << aBook.bookname << ' ' << aBook.price << ' ' << aBook.number << endl;
+		for (int i = 0; i < 5; ++i)
 		{
-			int m, n, a, b, c, d;
-			cin >> m >> n >> a >> b >> c >> d;
-#ifdef DEBUG
-			//cout << m << ' ' << n << ' ' << a << ' ' << b << ' ' << c << ' ' << d << endl;
-			span(m, n, a, b, c, d);
-			//cout << m << ' ' << n << ' ' << a << ' ' << b << ' ' << c << ' ' << d << endl;
-			reduce(m, n, a, b, c, d);
-			//cout << m << ' ' << n << ' ' << a << ' ' << b << ' ' << c << ' ' << d << endl;
-			double start = clock();
-			cout << search(m, n, a, b, c, d) << '\t';
-			cout << (clock() - start) / CLOCKS_PER_SEC << endl;
-#else
-			span(m, n, a, b, c, d);
-			reduce(m, n, a, b, c, d);
-			cout << search(m, n, a, b, c, d) << endl;
-#endif // DEBUG
-
-
-		}
-	}
-#ifdef DEBUG
-	in.close();
-	out.close();
-#endif // DEBUG
-
-}
-void span(int& m, int& n, int& a, int& b, int& c, int& d)
-{
-	auto dx = [&]()->int {return c - a; };
-	auto dy = [&]()->int {return d - b; };
-	if (dx() < 0)
-	{
-		if (dy() < 0)
-		{
-			a = m - 1 - a;
-			c = m - 1 - c;
-			b = n - 1 - b;
-			d = n - 1 - d;
-		}
-		else
-		{
-			a = m - 1 - a;
-			c = m - 1 - c;
-		}
-	}
-	else
-	{
-		if (dy() < 0)
-		{
-			b = n - 1 - b;
-			d = n - 1 - d;
-		}
-	}
-	if (dx())
-	{
-		;
-		if (dy() * 1.0 / dx() < 0.5)
-		{
-			swap(a, b);
-			swap(c, d);
-			swap(m, n);
-		}
-	}
-}
-void reduce(int& m, int& n, int& a, int& b, int& c, int& d)
-{
-	int max_size;
-	auto dx = [&]()->int {return c - a; };
-	auto dy = [&]()->int {return d - b; };
-	if (m > 10 && n > 10&&dx()>4)
-	{
-		max_size = 1;//>=1;
-	}
-	else
-	{
-		max_size = 3;
-	}
-	if (m > c + max_size&&dy()>4)
-	{
-		m = c + max_size;
-	}
-	if (n > d + max_size)
-	{
-		n = d + max_size;
-	}
-
-	if (a > max_size)
-	{
-		m -= a - max_size;
-		c -= a - max_size;
-		a = max_size;
-	}
-	if (b > max_size)
-	{
-		n -= b - max_size;
-		d -= b - max_size;
-		b = max_size;
-	}
-}
-int search(int m, int n, int a, int b, int c, int d)
-{
-	auto isCheck = [&](int x, int y)->bool {return ((x >= 0) && (x < m) && (y >= 0) && (y < n)); };
-	clr(S);
-	memset(L, UINT8_MAX, sizeof L);
-	Point Begin(a, b, 0), End(c, d, UINT_MAX);
-	priority_queue<Point, deque<Point>, greater<Point>> que;
-	que.push(Begin);
-	while ((!(que.empty())) && (que.top() != End))
-	{
-
-		Point t = que.top();
-		//p(t);
-		que.pop();
-
-		if (t.inS())
-		//if (S[t.x][t.y])
-		{
-			continue;
-		}
-		t.setS();
-		for (int i = 0; i < 8; ++i)
-		{
-			int x = t.x + horizontal[i];
-			int y = t.y + vertical[i];
-			unsigned step = t.step + 1;
-			if (S[x][y] || (L[x][y] <= step) || !(isCheck(x, y)))
+			cin >> op;
+			switch (op)
 			{
-				continue;
+			case 'B':
+				cout << aBook.borrow() << endl; break;
+			case 'R':
+				cout << aBook.restore() << endl; break;
+			case 'C':
+				cout << aBook.display() << endl; break;
+			default:
+				break;
 			}
-			Point temp(t, i);
-			//p(temp);
-			que.push(temp);
 		}
-	}
-	if (L[c][d] == 65535)
-	{
-		return 0;
-	}
-	else
-	{
-		return L[c][d];
 	}
 }
