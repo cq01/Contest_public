f646975caeda651092a61458dba4e20f53c79f88
cq01 <chq_github@outlook.com>
2020-03-06 00:25:58 +0800

    Homework 1842 - 2019级C++第八章探索性实验题(郑运平) 2773 Problem  D 4. 矩阵旋转

--- Contest.cpp
+++ Contest.cpp
@@ -4,64 +4,7 @@ using namespace std;
 using ll = long long;
 #define clr_0(A) memset((A),0,sizeof(A))
 //#define DEBUG
-
-class Integer
-{
-protected:
-	int n;
-};
-class Vector :public Integer
-{
-protected:
-	int* v;
-	//vector<int> v;
-public:
-	void set(int _n)
-	{
-		n = _n;
-		v = new int[n]();
-		//v.clear();
-		//v.resize(n);
-		for (int i = 0; i < n; ++i)cin >> v[i];
-	}
-	const Vector& print()const
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			if (i)cout << ' ';
-			cout << v[i];
-		}
-		cout << endl;
-		return *this;
-	}
-	Vector operator+(const Vector& ano)const
-	{
-		Vector ret;
-		ret.n = n;
-		ret.v = new int[n]();
-		//ret.v.resize(n);
-		for (int i = 0; i < n; ++i)ret.v[i] = v[i] + ano.v[i];
-		return ret;
-	}
-
-	Vector operator-(const Vector& ano)const
-	{
-		Vector ret;
-		ret.n = n;
-		ret.v = new int[n]();
-		//ret.v.resize(n);
-		for (int i = 0; i < n; ++i)ret.v[i] = v[i] - ano.v[i];
-		return ret;
-	}
-	int operator*(const Vector& ano)const
-	{
-		int ret = 0;
-		for (int i = 0; i < n; ++i)ret += v[i] * ano.v[i];
-		return ret;
-	}
-};
-
-
+int mat[500][500];
 int main()
 {
 #ifdef DEBUG
@@ -79,14 +22,17 @@ int main()
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	std::cout.tie(nullptr);
-	int num;
-	Vector v1, v2;
-	while (cin >> num)
+	int n;
+	while (cin >> n)
 	{
-		v1.set(num);
-		v2.set(num);
-		(v1 + v2).print();
-		(v1 - v2).print();
-		cout << v1 * v2 << endl;
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j)
+				cin >> mat[j][n - 1 - i];
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = 0; j < n; ++j)
+				cout << mat[i][j] << ' ';
+			cout << endl;
+		}
 	}
 }
