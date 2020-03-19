38696242e78049cb86a49a1a67adca2d2b0943e9
cq01 <chq_github@outlook.com>
2020-03-19 17:56:45 +0800

    Homework 1843 - 2019级C++第九章探索性实验题(郑运平) 2788 Problem  D 4. 报数

--- Contest.cpp
+++ Contest.cpp
@@ -4,89 +4,8 @@ using namespace std;
 using ll = long long;
 #define clr_0(A) memset((A),0,sizeof(A))
 //#define DEBUG
-class Employee
-{
-public:
-	char name[256];
-	Employee* next;
-	Employee() :name(), next() {}
-	virtual int pay() = 0;
-
-protected:
-	static int basic;
-};
-int Employee::basic = 2000;
-class Teacher :public Employee
-{
-	static int levelpay[3];
-public:
-	int level;
-	int classtime;
-	Teacher() :level(), classtime() {}
-	int pay()
-	{
-		return basic + classtime * levelpay[level - 1];
-	}
-};
-int Teacher::levelpay[3] = { 50,30,20 };
-class Admin :public Employee
-{
-public:
-	int work;
-	Admin() :work() {}
-	int pay()
-	{
-		return basic + work;
-	}
-};
-class Worker :public Employee
-{
-	static int payperday;
-public:
-	int day;
-	Worker() :day() {}
-	int pay()
-	{
-		return basic + payperday * day;
-	}
-};
-int Worker::payperday = 20;
-Employee* read()
-{
-	Employee* head = new Teacher;
-	Teacher* p = static_cast<Teacher*>(head);
-	Teacher* q = p;
-	while (cin >> p->name >> p->level >> p->classtime)
-	{
-		q = p;
-		p = static_cast<Teacher*>(p->next = new Teacher());
-	}
-	delete static_cast<Teacher*>(p);
-	q->next = nullptr;
-	return head;
-}
-void print(Employee* head)
-{
-	int sum = 0, cnt = 0, low = INT_MAX, high = INT_MIN;
-	for (auto p = head; p; p = p->next)
-	{
-		const int& ppay = p->pay();
-		sum += ppay;
-		++cnt;
-		low = min(low, ppay);
-		high = max(high, ppay);
-	}
-	cout << sum << endl << sum / cnt << endl << high << endl << low << endl;
-}
-void del(Employee* head)
-{
-	for (auto p = head; p; )
-	{
-		auto temp = p;
-		p = p->next;
-		delete temp;
-	}
-}
+constexpr int maxn = 1e6;
+int nxt[maxn];
 int main()
 {
 #ifdef DEBUG
@@ -104,7 +23,27 @@ int main()
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	std::cout.tie(nullptr);
-	Employee* head = read();
-	print(head);
-	del(head);
+	int n, m;
+	while (cin >> n >> m)
+	{
+		for (int i = 0; i < n; ++i)
+		{
+			nxt[i] = i + 1;
+		}
+		nxt[n - 1LL] = 0;
+		int cnt = n;
+		int cur = 0;
+		int call = 0;
+		while (cur != nxt[cur])
+		{
+			++call;
+			if (call >= m - 1)
+			{
+				call = 0;
+				nxt[cur] = nxt[nxt[cur]];
+			}
+			cur = nxt[cur];
+		}
+		cout << cur << endl;
+	}
 }
